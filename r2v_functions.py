73e596c0ef121d08db98006242b1590e158c8612
Bischoff <maik.bischoff@berliner-feuerwehr.de>
2024-05-13 07:16:17 +0200

    v1.03

--- r2v_functions.py
+++ r2v_functions.py
@@ -1,7 +1,7 @@
 import sqlite3
 import random
 import string
-from datetime import datetime
+from datetime import datetime, timedelta
 
 internal_id = None
 last_used_id = None
@@ -29,3 +29,16 @@ def log_step(process, internal_id, db_filename):
     with sqlite3.connect(db_filename) as conn:
         cursor = conn.cursor()
         cursor.execute("INSERT INTO protocol (timestamp, internal_id, process) VALUES (?, ?, ?)", (timestamp, internal_id, process))
+
+def clean_protocol(db_filename, weeks_to_keep):
+    # Berechne die Grenze des Zeitstempels für Einträge, die behalten werden sollen
+    current_time = datetime.now()
+    threshold_time = current_time - timedelta(weeks=weeks_to_keep)
+    threshold_timestamp = threshold_time.strftime("%Y-%m-%d %H:%M:%S")
+
+    # Lösche Einträge, die älter als die Grenze sind
+    with sqlite3.connect(db_filename) as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM protocol WHERE timestamp < ?", (threshold_timestamp,))
+        conn.commit()
+
